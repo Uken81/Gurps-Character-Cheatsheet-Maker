5ea93e83ad83006a2e2fe4b08b906e55b7776f75
Uken81 <64936770+Uken81@users.noreply.github.com>
2021-11-11 16:42:26 +1100

    Issue#30, Change the forms in the SignInAndSignUp components to use React Bootstrap.

--- src/Components/SignInAndSignUp/SignIn/sign-in-component.jsx
+++ src/Components/SignInAndSignUp/SignIn/sign-in-component.jsx
@@ -1,82 +1,97 @@
 import { useEffect, useState } from "react";
 
-import '../../../Pages/SignInAndSignUp/sign-in-and-sign-up.scss';
+import "../../../Pages/SignInAndSignUp/sign-in-and-sign-up.scss";
 
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useHistory } from "react-router";
 import { google } from "../../Firebase/firebase.utils";
+import { Button, Form } from "react-bootstrap";
 
 const SignIn = () => {
-    const [inputs, setInputs] = useState({});
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('');
+  const [inputs, setInputs] = useState({});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleChange = e => setInputs(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
+  const handleChange = (e) =>
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
 
-    const history = useHistory();
-    const auth = getAuth();
-    const handleSubmit = async event => {
-        event.preventDefault();
-        await signInWithEmailAndPassword(auth, email, password)
-        .then(() => {
-            console.log('****signin');
-            setInputs({ email: '', password: '' });
-            history.push("/create-or-manage-page");
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            console.log('error code: ', errorCode);
-            if (errorCode === 'auth/user-not-found') {
-                alert('The email you have entered has not been found.')
-            }
+  const history = useHistory();
+  const auth = getAuth();
 
-            if (errorCode === 'auth/wrong-password') {
-                alert('Wrong password entered for this account.')
-            }
-          });
-    }
-
-    useEffect(() => {
-        setEmail(inputs.email);
-        setPassword(inputs.password);
-    }, [inputs.email, inputs.password]);
-
-    const googleSignIn = async ()=> {
-        await google();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        console.log("****signin");
+        setInputs({ email: "", password: "" });
         history.push("/create-or-manage-page");
-    }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log("error code: ", errorCode);
+        if (errorCode === "auth/user-not-found") {
+          alert("The email you have entered has not been found.");
+        }
 
-    return (
-        <div className="sign-in">
-            <h1>I already have an account</h1>
-            <span>Sign in with your email and password</span>
+        if (errorCode === "auth/wrong-password") {
+          alert("Wrong password entered for this account.");
+        }
+      });
+  };
 
-            <form className="sign-in-form" onSubmit={handleSubmit}>           
-                    <input
-                        name='email'
-                        type='email'
-                        onChange={handleChange}
-                        placeholder='Email'
-                        value={email}
-                        required
-                    />
-       
-                    <input
-                        name='password'
-                        type='password'
-                        onChange={handleChange}
-                        placeholder='Password'
-                        value={password}
-                        required
-                    />
-           
-                <div className="buttons">
-                    <button className="submit" onClick={handleSubmit}>SIGN IN</button>
-                    <button className="google-sign-in" onClick={googleSignIn}>SIGN IN WITH GOOGLE</button>
-                </div>
-            </form>
-        </div>
-    );
-}
+  useEffect(() => {
+    setEmail(inputs.email);
+    setPassword(inputs.password);
+  }, [inputs.email, inputs.password]);
+
+  const googleSignIn = async () => {
+    await google();
+    history.push("/create-or-manage-page");
+  };
+
+  return (
+    <div className="sign-in">
+      <h1>I already have an account</h1>
+      <span>Sign in with your email and password</span>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group
+          className="sign-in-form"
+          controlId="sign-in-email"
+        >
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group
+          className="sign-in-form"         
+          controlId="sign-in-password"
+        >
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type='submit'>
+          SIGN IN
+        </Button>
+        <Button variant="primary" onClick={googleSignIn}>
+          SIGN IN WITH GOOGLE
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default SignIn;
--- src/Components/SignInAndSignUp/SignUp/sign-up-component.jsx
+++ src/Components/SignInAndSignUp/SignUp/sign-up-component.jsx
@@ -1,107 +1,124 @@
 import { useEffect, useState } from "react";
 
-import '../../../Pages/SignInAndSignUp/sign-in-and-sign-up.scss';
+import "../../../Pages/SignInAndSignUp/sign-in-and-sign-up.scss";
 
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useHistory } from "react-router";
-import { auth } from "../../Firebase/firebase.utils";
+import { auth, google } from "../../Firebase/firebase.utils";
+import { Button, Form } from "react-bootstrap";
 
 const SignUp = () => {
-    const [inputs, setInputs] = useState({});
-    const [displayName, setDisplayName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+  const [inputs, setInputs] = useState({});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-    const history = useHistory();
+  const history = useHistory();
 
-    const handleChange = e => setInputs(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
+  const handleChange = (e) =>
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
 
-    const handleSubmit = async event => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        if (password !== confirmPassword) {
-            alert("passwords don't match");
-            return;
-        }
-
-        try {
-            await createUserWithEmailAndPassword(auth, email, password);
-
-            setInputs({ displayName: '', email: '', password: '', confirmPassword: '' });
-            history.push('/create-or-manage-page');
-        } catch (error) {
-            console.error('error code: ', error.code);
-            const errorCode = error.code;
-
-            if (errorCode === 'auth/email-already-in-use') {
-                alert('An account using this email already exists')
-            }
-
-            if (errorCode === 'auth/invalid-email') {
-                alert('The provided Email is invalid')
-            }
-            
-            if (errorCode === 'auth/weak-password') {
-                alert('The password is too weak');
-            }
-        }
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
     }
 
-    useEffect(() => {
-        console.log('***Test: sign-up-component/form onChange');
-        setDisplayName(inputs.displayName);
-        setEmail(inputs.email);
-        setPassword(inputs.password);
-        setConfirmPassword(inputs.confirmPassword);
-    }, [inputs.confirmPassword, inputs.displayName, inputs.email, inputs.password]);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
 
-    return (
-        <div className='sign-up'>
-            <h1>I do not have an account</h1>
-            <span>Sign up with your email and password</span>
-            <form className='sign-up-form' onSubmit={handleSubmit}>
+      setInputs({
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+      history.push("/create-or-manage-page");
+    } catch (error) {
+      console.error("error code: ", error.code);
+      const errorCode = error.code;
 
-                <input
-                    type='text'
-                    name='displayName'
-                    value={displayName}
-                    onChange={handleChange}
-                    placeholder='Display Name'
-                    required
-                />
+      if (errorCode === "auth/email-already-in-use") {
+        alert("An account using this email already exists");
+      }
 
-                <input
-                    type='email'
-                    name='email'
-                    value={email}
-                    onChange={handleChange}
-                    placeholder='Email'
-                    required
-                />
+      if (errorCode === "auth/invalid-email") {
+        alert("The provided Email is invalid");
+      }
 
-                <input
-                    type='password'
-                    name='password'
-                    value={password}
-                    onChange={handleChange}
-                    placeholder='Password'
-                    required
-                />
+      if (errorCode === "auth/weak-password") {
+        alert("The password is too weak");
+      }
+    }
+  };
 
-                <input
-                    type='password'
-                    name='confirmPassword'
-                    value={confirmPassword}
-                    onChange={handleChange}
-                    placeholder='Confirm Password'
+  useEffect(() => {
+    console.log("***Test: sign-up-component/form onChange");
+    setEmail(inputs.email);
+    setPassword(inputs.password);
+    setConfirmPassword(inputs.confirmPassword);
+  }, [
+    inputs.confirmPassword,
+    inputs.displayName,
+    inputs.email,
+    inputs.password,
+  ]);
 
-                    required
-                />
-                <button type='submit'>SIGN UP</button>
-            </form>
-        </div>
-    );
-}
+  const googleSignUp = async () => {
+    await google();
+    history.push("/create-or-manage-page");
+  };
+
+  return (
+    <div className="sign-up">
+      <h1>I do not have an account</h1>
+      <span>Sign up with your email and password</span>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="sign-up-form" controlId="sign-up-email">
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group className="sign-up-form" controlId="sign-up-password">
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+          />
+        </Form.Group>
+
+        <Form.Group className="sign-up-form" controlId="confirm-password">
+          <Form.Control
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            placeholder="Confirm Password"
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          SIGN UP
+        </Button>
+        <Button variant="primary" onClick={googleSignUp}>
+          SIGN UP WITH GOOGLE
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default SignUp;
