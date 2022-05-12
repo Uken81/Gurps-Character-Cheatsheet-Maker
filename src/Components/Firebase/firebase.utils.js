befa24c631aa58fc79be095e89e79ef7fcb9baa6
Uken81 <64936770+Uken81@users.noreply.github.com>
2022-05-12 15:29:27 +1000

    Test if key is coming from .env file or netlify

--- src/Components/Firebase/firebase.utils.js
+++ src/Components/Firebase/firebase.utils.js
@@ -16,7 +16,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSENGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
-
+//test
 const firebaseApp = initializeApp(firebaseConfig);
 
 const db = getFirestore(firebaseApp);
